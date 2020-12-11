884d983b1a7c30ade39030b4169b962c7f96dac9
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-12-11 18:35:55 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,41 @@ struct TodoItem {
     completed: char
 }
 
+impl TodoItem {
+    fn new(name: String) -> TodoItem {
+        return TodoItem{
+            name: name,
+            completed: ' '
+        };
+    }
+}
+
+struct TodoList {
+    list: Vec<TodoItem>
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        return TodoList{list: Vec::new()}
+    }
+
+    fn add_to_list(&mut self, name: String) {
+        self.list.push(TodoItem::new(name))
+    }
+}
+
+
+
 fn main() {
     let arguments: Vec<String> = env::args().collect();
     let command = arguments[1].clone();
-    let new_todo = TodoItem{
-        name: "Say hi!".to_string(),
-        completed: ' '
-    };
-    let todo_list = vec![new_todo];
+    let mut todo_list = TodoList::new();
+
+    todo_list.add_to_list("Say hi!".to_string());
+    todo_list.add_to_list("Say Bye!".to_string());
 
     if command == "get" {
-        for todo in todo_list {
+        for todo in todo_list.list {
             println!("[{}] - {}", todo.completed, todo.name)
         }
     }
